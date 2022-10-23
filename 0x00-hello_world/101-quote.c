a3cb44b20b014dac56808fbe027227058e508ecd
Tychool <ok.hezekiahs@gmail.com>
2022-10-23 10:33:16 -0700

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Prints "and that piece of art is usefull" - Dora Kopar, 2015-10-19"
- * 	Followed by a new line to standard error.
+ * Followed by a new line to standard error.
  *
  * Return: always 1.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Kompar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Kompar, 2015-10-19\n", 59);
 	return (1);
 }
