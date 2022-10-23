20e5fece8f81838672b64d76f94e7f7a02b87e4f
Tychool <ok.hezekiahs@gmail.com>
2022-10-23 10:48:01 -0700

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - Prints "and that piece of art is usefull" - Dora Kopar, 2015-10-19"
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Kompar, 2015-10-19\n", 59);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Kompar, 2015-10-19\n");
 	return (1);
 }
